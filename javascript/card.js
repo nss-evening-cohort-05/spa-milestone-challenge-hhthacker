5a8e4e64c64eed6dbb487b89afecf15beb21fc10
Heather Thacker <hhthacker@gmail.com>
2017-04-03 21:18:43 -0500

    mirroring input text in description

--- javascript/card.js
+++ javascript/card.js
@@ -3,7 +3,7 @@
 
 var CarLot = (function (cars) {
 		var textInput = document.getElementById("carDescription");
-		var editScribe = document.querySelectorAll('p[id^="redescribe-"]');
+		var editScribe = "";
 		var selectedCar;
 
 	cars.resetStyle = function (event) {
@@ -11,28 +11,72 @@ var CarLot = (function (cars) {
 	},
 
 	cars.setStyle = function (event) {
-		console.log("set style", event);
-		if (event.target.parentNode.classList.contains("noclicky")) {
+		var carClasses = document.getElementsByClassName("thumbnail");
+		for (var x = 0; x < 6; x++) {
+
+		if (event.target.classList.contains("describe")) {
+			textInput.focus();
+			console.log("no event1", event.target.parentNode.classList);
+
+			selectedCar = event.target;
 			event.target.parentNode.classList.remove("noclicky");
-			event.target.parentNode.classList.add("clicky");
-			event.target.
-			//event.target.
+			event.target.parentNode.parentNode.classList.add("clicky");
+
+		} else if (event.target.classList.contains("grandchild")) {
 			textInput.focus();
-			editScribe.innerHTML = textInput.value;
-		} else if (event.target.parentNode.classList.contains("thumbnail")) {
+			console.log("no event2", event.target.parentNode.classList);
+
+			selectedCar = event.target.parentNode.childNodes[3];
+			event.target.parentNode.classList.remove("noclicky");			
+			event.target.parentNode.parentNode.classList.add("clicky");
+
+		} else if (event.target.classList.contains("child")) {
+			textInput.focus();
+			console.log("no event3", event.target.classList);
+
+			selectedCar = event.target.childNodes[3];
 			event.target.classList.remove("noclicky");
-			event.target.classList.add("clicky");
-			textInput.focus();
-		}
-		textInput.addEventListener("keyup", CarLot.enterStyle);
-	},
+			event.target.parentNode.classList.add("clicky");
+		};
+		textInput.value = selectedCar.innerHTML;
+		textInput.addEventListener("keyup", function(){
+			textInput.onkeydown = function (){
+        if (window.event.keyCode === 13) {
+          textInput.value = "";
+        } else {
+          selectedCar.innerHTML = textInput.value;
+        };
+      };
+	});
+};
+},
+
+
+
+
+		// if (event.target.parentNode.classList.contains("noclicky")) {
+		// 	event.target.parentNode.classList.remove("noclicky");
+		// 	event.target.parentNode.classList.add("clicky");
+		// 	//event.target.
+		// 	textInput.focus();
+		// 	textInput.value = "";
+		// 	event.target.innerHTML = editScribe;
+		// 	textInput.value = editScribe;
+		// 	//CarLot.enterStyle();
+
+		// } else if (event.target.parentNode.classList.contains("thumbnail")) {
+		// 	event.target.classList.remove("noclicky");
+		// 	event.target.classList.add("clicky");
+		// 	textInput.focus();
+		// }
+		// textInput.addEventListener("keyup", CarLot.enterStyle);
 
 	cars.enterStyle = function (event) {
       textInput.onkeydown = function (){
         if (window.event.keyCode === 13) {
           textInput.value = "";
         } else {
-          editScribe.innerHTML = textInput.value;
+         textInput.value = editScribe;
 			};
 		};
 	}
--- javascript/carlot.js
+++ javascript/carlot.js
@@ -3,16 +3,20 @@ var CarLot = (function (cars) {
 //public function: parse xhr request into object array & loads the `inventory.json` file 
 //stores the inventory in a private variable. 
   return {
+
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
       inventoryLoader.addEventListener("load", CarLot.parseInventory);
       inventoryLoader.open("GET", "javascript/inventory.json");
       inventoryLoader.send(); 
     },
-    parseInventory: function () {
+    parseInventory: function (inventory) {
       inventory = JSON.parse(this.responseText);
       populatePage(inventory);
-    }
+    },
+    getInventory: function () {
+    return inventory;
+  }
   };
 
 })(CarLot || {});
